206b5fb8e32b23121f0fd2b515b4b615008dd3b5
Hades <wzawzpopo@qq.com>
2022-11-19 10:22:08 +0800

    修改macos支持版本,去掉UIKit

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     platforms: [
         .iOS(.v14),
         .tvOS(.v14),
-        .macOS(.v12),
+        .macOS(.v11),
     ],
     products: [
         .library(name: "IPTVKit", targets: ["IPTVKit"]),
